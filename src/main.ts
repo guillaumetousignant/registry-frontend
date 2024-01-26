5970f4234ee6859ea93aefb8f2c1e6a39870260d
Guillaume Tousignant <moi_guillaume@hotmail.com>
2024-01-25 23:06:37 -0500

    Fixed bug introduced last commit that prevented scrolling on small screens

--- src/main.ts
+++ src/main.ts
@@ -61,7 +61,7 @@ import enCA from './locales/en-CA.json'
 type MessageSchema = typeof enCA
 
 const usersLanguage = window.navigator.language
-const usersLocale = usersLanguage.startsWith("fr") ? 'fr-CA' : 'en-CA'
+const usersLocale = usersLanguage.startsWith('fr') ? 'fr-CA' : 'en-CA'
 const i18n = createI18n<[MessageSchema], 'en-CA' | 'fr-CA'>({
     inheritLocale: true,
     legacy: false, // you must set `false`, to use Composition API
