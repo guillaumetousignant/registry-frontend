e88c6d99b317500c464447b0f943e37162e37442
Guillaume Tousignant <moi_guillaume@hotmail.com>
2026-05-20 13:07:02 -0400

    CUstomisation for poupouchou

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,24 +7,31 @@ const { t } = useI18n()
     <div class="about" style="max-width: 1280; min-width: 128; padding: 16px 16px 16px 16px">
         <h1 class="font-weight-thin">{{ t('about.title') }}</h1>
         <p class="font-weight-light">
-            <b>{{ t('about.p4') }}</b>
+            <b>{{ t('about.p5') }}</b>
         </p>
         <p class="font-weight-light">
             <b>{{ t('about.p1') }}</b>
             <a
                 class="font-weight-medium"
-                href="mailto:ari@arietguillaume.ca"
+                href="mailto:desiree.champagne-charles@hotmail.com"
                 style="text-decoration: none"
-                >ari@arietguillaume.ca</a
+                >desiree.champagne-charles@hotmail.com</a
             >
             <b>{{ t('about.p2') }}</b>
+            <a
+                class="font-weight-medium"
+                href="mailto:moi_felixtou@hotmail.com"
+                style="text-decoration: none"
+                >moi_felixtou@hotmail.com</a
+            >
+            <b>{{ t('about.p3') }}</b>
             <a
                 class="font-weight-medium"
                 href="mailto:guillaume@arietguillaume.ca"
                 style="text-decoration: none"
                 >guillaume@arietguillaume.ca</a
             >
-            <b>{{ t('about.p3') }}</b>
+            <b>{{ t('about.p4') }}</b>
         </p>
     </div>
 </template>
